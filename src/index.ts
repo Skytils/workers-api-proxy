a0026e77289c008fc91c89717afbb956be3b304f
sychic <47618543+Sychic@users.noreply.github.com>
2025-02-04 08:26:10 -0500

    feat: use cache api

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@ export default {
 		let url = new URL(request.url);
 		url.host = env.ROOT;
 		url.port = "";
-		let response = await fetch(url, {
-			cf: {
-				cacheEverything: true,
-				cacheTtlByStatus: {
-					"200-299": 3600,
-					"429": 0
+		let cache = caches.default;
+		let response = await cache.match(url);
+		if (!response) {
+			response = await fetch(url, {
+				cf: {
+					cacheEverything: true,
+					cacheTtlByStatus: {
+						"200-299": 3600,
+						"429": 0
+					}
+				},
+				headers: {
+					"API-Key": env.API_KEY
 				}
-			},
-			headers: {
-				"API-Key": env.API_KEY
-			}
-		});
-		response = new Response(response.body);
-		response.headers.delete("cache-control");
+			});
+			response = new Response(response.body);
+			response.headers.delete("cache-control");
+			await cache.put(url, response.clone());
+		}
 		return response;
 	},
 } satisfies ExportedHandler<Env>;
